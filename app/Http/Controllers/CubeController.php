c8163aca0c11712472944650e635b90c427a658a
Edoardo <a@Air-di-edo.lan>
2019-09-30 16:05:52 +0200

    Aggiunti dati bevande su schermo

--- app/Http/Controllers/CubeController.php
+++ app/Http/Controllers/CubeController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Drink;
 
 
 class CubeController extends Controller
 {
     public function getCubes(){
 
+      $values = Drink::all();
+      $min = $values->min('prezzo');
+      $max = $values->max('prezzo');
+      $avg = $values->avg('prezzo');
 
-      return view('cubes');
+
+      return view('cubes', compact('values', 'min', 'max','avg'));
     }
 }
--- resources/views/cubes.blade.php
+++ resources/views/cubes.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.baselayout')
 
 @section('content')
-  Hello world
+  <div class="drinks container">
+    @foreach ($values as $value)
+      <div class="cube">
+        {{$value->nome_bevanda}} <br>
+        {{$value->marca}} <br>
+        {{$value->prezzo}}€
+      </div>
+    @endforeach
+  </div>
+
+  <div class="stats container">
+    <div class="cube">
+      Prezzo massimo: {{$value->prezzo}}€
+    </div>
+    <div class="cube">
+      Prezzo minimo: {{$value->prezzo}}€
+    </div>
+    <div class="cube">
+      Prezzo medio: {{$value->prezzo}}€
+    </div>
+  </div>
 @endsection
